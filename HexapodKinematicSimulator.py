028c9b8938bc4d0ecdc8b2046f5f16463c99dc6c
M-Ghodrat <ghodrat@ualberta.ca>
2022-06-20 13:56:32 -0600

    update

--- HexapodKinematicSimulator.py
+++ HexapodKinematicSimulator.py
@@ -178,6 +178,118 @@ def reset_geo():
     
 with st.sidebar:
     
+    angles = np.array([[0.0], [0.0], [0.0]])
+    linears = np.array([[0.0], [0.0], [0.0]])
+    angles_0 = angles.copy()
+    
+    b_i = np.array([[st.session_state['act1_lower_x_new'], 
+                     st.session_state['act2_lower_x_new'], 
+                     st.session_state['act3_lower_x_new'], 
+                     st.session_state['act4_lower_x_new'], 
+                     st.session_state['act5_lower_x_new'], 
+                     st.session_state['act6_lower_x_new']],
+                    [st.session_state['act1_lower_y_new'], 
+                     st.session_state['act2_lower_y_new'], 
+                     st.session_state['act3_lower_y_new'], 
+                     st.session_state['act4_lower_y_new'], 
+                     st.session_state['act5_lower_y_new'], 
+                     st.session_state['act6_lower_y_new']],
+                    [st.session_state['act1_lower_z_new'], 
+                     st.session_state['act2_lower_z_new'], 
+                     st.session_state['act3_lower_z_new'], 
+                     st.session_state['act4_lower_z_new'], 
+                     st.session_state['act5_lower_z_new'], 
+                     st.session_state['act6_lower_z_new']]])
+
+    u_s = np.array([[st.session_state['act1_upper_x_new'], 
+                     st.session_state['act2_upper_x_new'], 
+                     st.session_state['act3_upper_x_new'], 
+                     st.session_state['act4_upper_x_new'], 
+                     st.session_state['act5_upper_x_new'], 
+                     st.session_state['act6_upper_x_new']],
+                    [st.session_state['act1_upper_y_new'], 
+                     st.session_state['act2_upper_y_new'], 
+                     st.session_state['act3_upper_y_new'], 
+                     st.session_state['act4_upper_y_new'], 
+                     st.session_state['act5_upper_y_new'], 
+                     st.session_state['act6_upper_y_new']],
+                    [st.session_state['act1_upper_z_new'], 
+                     st.session_state['act2_upper_z_new'], 
+                     st.session_state['act3_upper_z_new'], 
+                     st.session_state['act4_upper_z_new'], 
+                     st.session_state['act5_upper_z_new'], 
+                     st.session_state['act6_upper_z_new']]])
+    
+    r_si_0 = np.array([[0], [0], [-st.session_state.dist_frame_new]])
+    r_si = r_si_0 + linears
+    
+    value_final = np.array([[0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.0]])
+    dict_modes = {'surge':0, 'sway':1, 'heave':2, 'roll':3, 'pitch':4, 'yaw':5}
+    dict_increment = {'min':0, 'max':1}
+    act_len = np.zeros((6,6,2))
+
+    PIV = np.array([[st.session_state.pivot_x], [st.session_state.pivot_y], [-st.session_state.pivot_z]])
+    PIV_ext = np.array([[st.session_state.pivot_x], [st.session_state.pivot_y], [st.session_state.pivot_z]]).repeat(len(u_s[0]), axis=0).reshape(-1,len(u_s[0]))
+
+    u_s_new = u_s - PIV_ext
+
+    for j in dict_increment.keys():
+
+        if j == 'min':
+            increment = -0.1
+        else:
+            increment = +0.1
+
+        for i in dict_modes.keys():
+
+            if i in {'roll', 'pitch', 'yaw'}:
+                increment /= 10
+
+            flag = True
+            value = np.array([[0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.0]])
+
+            while flag == True:
+
+                linears = np.array([[value[dict_modes['surge']][dict_increment[j]]], [value[dict_modes['sway']][dict_increment[j]]], [-value[dict_modes['heave']][dict_increment[j]]]])
+                angles = np.array([[value[dict_modes['roll']][dict_increment[j]]], [value[dict_modes['pitch']][dict_increment[j]]], [value[dict_modes['yaw']][dict_increment[j]]]])
+#                 r_si = np.array([r_si_0[0], r_si_0[1], -r_si_0[2]]) + PIV + linears
+                r_si = r_si_0 + PIV + linears
+                actuator_lengths = np.array(actuator_length(angles, u_s_new, b_i, r_si))/st.session_state.unit
+
+                if (actuator_lengths.min() > st.session_state.minL_new/st.session_state.unit) & (actuator_lengths.max() < st.session_state.maxL_new/st.session_state.unit):    
+                    value[dict_modes[i]][dict_increment[j]] += increment
+                else:
+                    flag = False   
+
+            act_len[:,dict_modes[i],dict_increment[j]] = actuator_lengths
+            value[dict_modes[i]][dict_increment[j]] -= increment
+
+            value_final[dict_modes[i]][dict_increment[j]] = value[dict_modes[i]][dict_increment[j]]
+
+    for i in range(6):
+        if i < 3:
+            value_final[i] = value_final[i]#*100
+        else:
+            value_final[i] = value_final[i]*180/np.pi
+
+    value_final_new = value_final.copy()
+
+    value_final_new = np.round(value_final_new, 1)/st.session_state.unit
+    Admissible = pd.DataFrame(value_final_new, columns=['minimum range', 'maximum range'], index=['Surge', 'Sway', 'Heave', 'Roll', 'Pitch', 'Yaw'])
+    
+    @st.cache
+    def convert_df(df):
+        return df.to_csv().encode('utf-8')
+    
+    csv = convert_df(Admissible)
+    st.download_button(
+        "Press to Download the Results",
+        csv,
+        "output.csv",
+        "text/csv",
+        key='download-csv'
+    )
+    
     uploaded_file = st.file_uploader("Choose a .XLSX file", accept_multiple_files=False)
     if uploaded_file is not None:
         df = pd.read_excel(uploaded_file)        
@@ -248,50 +360,50 @@ with st.sidebar:
             st.session_state[val] = st.session_state[val+'_new']
             
     ####
-    angles = np.array([[0.0], [0.0], [0.0]])
-    linears = np.array([[0.0], [0.0], [0.0]])
-    angles_0 = angles.copy()
+#     angles = np.array([[0.0], [0.0], [0.0]])
+#     linears = np.array([[0.0], [0.0], [0.0]])
+#     angles_0 = angles.copy()
     
-    b_i = np.array([[st.session_state['act1_lower_x_new'], 
-                     st.session_state['act2_lower_x_new'], 
-                     st.session_state['act3_lower_x_new'], 
-                     st.session_state['act4_lower_x_new'], 
-                     st.session_state['act5_lower_x_new'], 
-                     st.session_state['act6_lower_x_new']],
-                    [st.session_state['act1_lower_y_new'], 
-                     st.session_state['act2_lower_y_new'], 
-                     st.session_state['act3_lower_y_new'], 
-                     st.session_state['act4_lower_y_new'], 
-                     st.session_state['act5_lower_y_new'], 
-                     st.session_state['act6_lower_y_new']],
-                    [st.session_state['act1_lower_z_new'], 
-                     st.session_state['act2_lower_z_new'], 
-                     st.session_state['act3_lower_z_new'], 
-                     st.session_state['act4_lower_z_new'], 
-                     st.session_state['act5_lower_z_new'], 
-                     st.session_state['act6_lower_z_new']]])
+#     b_i = np.array([[st.session_state['act1_lower_x_new'], 
+#                      st.session_state['act2_lower_x_new'], 
+#                      st.session_state['act3_lower_x_new'], 
+#                      st.session_state['act4_lower_x_new'], 
+#                      st.session_state['act5_lower_x_new'], 
+#                      st.session_state['act6_lower_x_new']],
+#                     [st.session_state['act1_lower_y_new'], 
+#                      st.session_state['act2_lower_y_new'], 
+#                      st.session_state['act3_lower_y_new'], 
+#                      st.session_state['act4_lower_y_new'], 
+#                      st.session_state['act5_lower_y_new'], 
+#                      st.session_state['act6_lower_y_new']],
+#                     [st.session_state['act1_lower_z_new'], 
+#                      st.session_state['act2_lower_z_new'], 
+#                      st.session_state['act3_lower_z_new'], 
+#                      st.session_state['act4_lower_z_new'], 
+#                      st.session_state['act5_lower_z_new'], 
+#                      st.session_state['act6_lower_z_new']]])
 
-    u_s = np.array([[st.session_state['act1_upper_x_new'], 
-                     st.session_state['act2_upper_x_new'], 
-                     st.session_state['act3_upper_x_new'], 
-                     st.session_state['act4_upper_x_new'], 
-                     st.session_state['act5_upper_x_new'], 
-                     st.session_state['act6_upper_x_new']],
-                    [st.session_state['act1_upper_y_new'], 
-                     st.session_state['act2_upper_y_new'], 
-                     st.session_state['act3_upper_y_new'], 
-                     st.session_state['act4_upper_y_new'], 
-                     st.session_state['act5_upper_y_new'], 
-                     st.session_state['act6_upper_y_new']],
-                    [st.session_state['act1_upper_z_new'], 
-                     st.session_state['act2_upper_z_new'], 
-                     st.session_state['act3_upper_z_new'], 
-                     st.session_state['act4_upper_z_new'], 
-                     st.session_state['act5_upper_z_new'], 
-                     st.session_state['act6_upper_z_new']]])
+#     u_s = np.array([[st.session_state['act1_upper_x_new'], 
+#                      st.session_state['act2_upper_x_new'], 
+#                      st.session_state['act3_upper_x_new'], 
+#                      st.session_state['act4_upper_x_new'], 
+#                      st.session_state['act5_upper_x_new'], 
+#                      st.session_state['act6_upper_x_new']],
+#                     [st.session_state['act1_upper_y_new'], 
+#                      st.session_state['act2_upper_y_new'], 
+#                      st.session_state['act3_upper_y_new'], 
+#                      st.session_state['act4_upper_y_new'], 
+#                      st.session_state['act5_upper_y_new'], 
+#                      st.session_state['act6_upper_y_new']],
+#                     [st.session_state['act1_upper_z_new'], 
+#                      st.session_state['act2_upper_z_new'], 
+#                      st.session_state['act3_upper_z_new'], 
+#                      st.session_state['act4_upper_z_new'], 
+#                      st.session_state['act5_upper_z_new'], 
+#                      st.session_state['act6_upper_z_new']]])
     
-    r_si_0 = np.array([[0], [0], [-st.session_state.dist_frame_new]])
-    r_si = r_si_0 + linears
+#     r_si_0 = np.array([[0], [0], [-st.session_state.dist_frame_new]])
+#     r_si = r_si_0 + linears
     fig = plt.figure(figsize=(5,5))
     ax = plt.axes(projection='3d')
 #     ax._axis3don = False
